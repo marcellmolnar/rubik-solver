edfec0e8e9694a1615d9665518804f41473331a8
Marcell Molnár <marcell.molnar97@gmail.com>
2024-08-19 14:16:08 +0200

    brute force solver added for limited number of moves

--- src/cubesolver.rs
+++ src/cubesolver.rs
@@ -37,22 +37,71 @@ fn rotate_cube(cube: &mut RubiksCube, rotation: i32) {
     }
 }
 
-pub fn solve_cube(cube: &mut RubiksCube) -> String {
+fn undo_move(cube: &mut RubiksCube, rotation: i32) {
+    rotate_cube(cube, if rotation % 2 == 0 { rotation + 1 } else { rotation - 1 });
+}
+
+// return string of the moves from vector of integers
+fn moves_to_string(moves: &Vec<i32>) -> String {
+    let mut result = "".to_string();
+    for i in 0..moves.len() {
+        result.push_str(&rotation_to_string(moves[i]));
+        result.push_str(" ");
+    }
+    return result;
+}
+
+fn solve_cube_brute_force(cube: &mut RubiksCube) -> String {
     if cube.is_solved() {
         return "".to_string();
     }
     
-    for rotation in 0..12 {
-        rotate_cube(cube, rotation);
+    for rot1 in 0..12 {
+        rotate_cube(cube, rot1);
         if cube.is_solved() {
-            return rotation_to_string(rotation);
+            return moves_to_string(&vec![rot1]);
         }
-
-        // undo the move
-        rotate_cube(cube, if rotation % 2 == 0 { rotation + 1 } else { rotation - 1 });
+        for rot2 in 0..12 {
+            rotate_cube(cube, rot2);
+            if cube.is_solved() {
+                return moves_to_string(&vec![rot1, rot2]);
+            }
+            for rot3 in 0..12 {
+                rotate_cube(cube, rot3);
+                if cube.is_solved() {
+                    return moves_to_string(&vec![rot1, rot2, rot3]);
+                }
+                for rot4 in 0..12 {
+                    rotate_cube(cube, rot4);
+                    if cube.is_solved() {
+                        return moves_to_string(&vec![rot1, rot2, rot3, rot4]);
+                    }
+                    for rot5 in 0..12 {
+                        rotate_cube(cube, rot5);
+                        if cube.is_solved() {
+                            return moves_to_string(&vec![rot1, rot2, rot3, rot4, rot5]);
+                        }
+                        for rot6 in 0..12 {
+                            rotate_cube(cube, rot6);
+                            if cube.is_solved() {
+                                return moves_to_string(&vec![rot1, rot2, rot3, rot4, rot5, rot6]);
+                            }
+                            undo_move(cube, rot6);
+                        }
+                        undo_move(cube, rot5);
+                    }
+                    undo_move(cube, rot4);
+                }
+                undo_move(cube, rot3);
+            }
+            undo_move(cube, rot2);
+        }
+        undo_move(cube, rot1);
     }
 
-    for step in 0..20 {
-    }
-    return "".to_string();
+    return "not solved".to_string();
+}
+
+pub fn solve_cube(cube: &mut RubiksCube) -> String {
+    return solve_cube_brute_force(cube);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 mod rubikscube;
 mod cubesolver;
 
@@ -5,14 +7,19 @@ fn main() {
     println!("Hello, world!");
     println!("This is a Rubik's cube solver");
     
-    let mut cube = rubikscube::RubiksCube::new();
-    cube.L();
+    let mut cube: rubikscube::RubiksCube = rubikscube::RubiksCube::new();
 
-    let soluton = cubesolver::solve_cube(&mut cube);
-    println!("Solution: {}", soluton);
+    let mut sum_duration: u128 = 0;
+    let iters: u128 = 50;
+    let scarmble_moves = 6;
+    for _i in 0..iters {
+        let scramble: String = cube.scramble(scarmble_moves);
+        //println!("Scramble: {}", scramble);
 
-    // scramble cube
-    let moves = 20;
-    let scramble = cube.scramble(moves);
-    println!("Scramble: {}", scramble);
+        let now: Instant = Instant::now();
+        cubesolver::solve_cube(&mut cube);
+        let end: Duration = now.elapsed();
+        sum_duration += end.as_millis();
+    }
+    println!("avg solve time for {} moves is {} ms", scarmble_moves, sum_duration/iters);
 }

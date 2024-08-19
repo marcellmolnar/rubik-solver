203b4176fb3513f6bbc7dee13f60aec87741676f
Marcell Molnár <marcell.molnar97@gmail.com>
2024-08-19 19:56:50 +0200

    some function moved

--- src/cubesolver.rs
+++ src/cubesolver.rs
@@ -1,45 +1,6 @@
-use rand::Rng;
+use super::common_functions::{rotation_to_string, rotate_cube, undo_move};
+
 use crate::rubikscube::RubiksCube;
-
-fn rotation_to_string(rotation: i32) -> String {
-    match rotation {
-        0 => "U".to_string(),
-        1 => "U'".to_string(),
-        2 => "D".to_string(),
-        3 => "D'".to_string(),
-        4 => "R".to_string(),
-        5 => "R'".to_string(),
-        6 => "L".to_string(),
-        7 => "L'".to_string(),
-        8 => "F".to_string(),
-        9 => "F'".to_string(),
-        10 => "B".to_string(),
-        11 => "B'".to_string(),
-        _ => panic!("Invalid move number"),
-    }
-}
-
-fn rotate_cube(cube: &mut RubiksCube, rotation: i32) {
-    match rotation {
-        0 => cube.U(),
-        1 => cube.Up(),
-        2 => cube.D(),
-        3 => cube.Dp(),
-        4 => cube.R(),
-        5 => cube.Rp(),
-        6 => cube.L(),
-        7 => cube.Lp(),
-        8 => cube.F(),
-        9 => cube.Fp(),
-        10 => cube.B(),
-        11 => cube.Bp(),
-        _ => panic!("Invalid move number"),
-    }
-}
-
-fn undo_move(cube: &mut RubiksCube, rotation: i32) {
-    rotate_cube(cube, if rotation % 2 == 0 { rotation + 1 } else { rotation - 1 });
-}
 
 // return string of the moves from vector of integers
 fn moves_to_string(moves: &Vec<i32>) -> String {
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::time::{Duration, Instant};
 
 mod rubikscube;
 mod cubesolver;
-
+mod common_functions;
 
 fn test_solve_time(cube: &mut rubikscube::RubiksCube, scarmble_moves: i32) {
     let iters: u128 = 50;
@@ -26,6 +26,17 @@ fn main() {
     let mut cube: rubikscube::RubiksCube = rubikscube::RubiksCube::new();
 
     for scarmble_moves in 1..9 {
-        test_solve_time(&mut cube, scarmble_moves);
+        //test_solve_time(&mut cube, scarmble_moves);
     }
+    //test_solve_time(&mut cube, 10);
+
+    
+    let scramble: String = cube.scramble(6);
+    println!("Scramble: {}", scramble);
+
+    let now: Instant = Instant::now();
+    let sol = cubesolver::solve_cube(&mut cube);
+    println!("Solution: {}", sol);
+    let end: Duration = now.elapsed();
+    println!("Solve time: {} ms", end.as_millis());
 }
